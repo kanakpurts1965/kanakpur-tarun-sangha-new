de986ed5050dec7c37bc72c923027329cc50c854
kanakpurts1965 <kanakpurts1965@gmail.com>
2026-06-28 17:29:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,5 @@
-alert("Step 1");
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-
-import {
-  getFirestore,
-  collection,
-  addDoc
-} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
-
-alert("Step 2");
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCSxn1dAA708KyKOfrLP_nTrBnazrFjPT8",
@@ -19,38 +10,11 @@ const firebaseConfig = {
   appId: "1:1062615905207:web:53e520f0696aeb0daa6e32"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-alert("Step 3");
-
+// Initialize Firestore
 const db = getFirestore(app);
 
-alert("Step 4");
-
-try {
-
-  const docRef = await addDoc(collection(db, "comments"), {
-
-    name: "Test User",
-
-    contact: "6296062310",
-
-    comment: "Firebase Test Successful",
-
-    page: "Home",
-
-    time: new Date()
-
-  });
-
-  alert("✅ Firestore Write Success");
-
-  console.log("Document ID:", docRef.id);
-
-} catch (e) {
-
-  alert("❌ Firestore Error");
-
-  console.error(e);
-
-}
+// Export Firestore
+export { db };
