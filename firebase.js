67d2f889770ab52ebeb30202ee0d68da04e5f0a9
kanakpurts1965 <kanakpurts1965@gmail.com>
2026-07-03 22:06:38 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,70 +1,35 @@
-// ==========================================
-// FIREBASE CONFIGURATION
-// FIRESTORE + STORAGE
-// ==========================================
-
 import {
     initializeApp
 } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-
 
 import {
     getFirestore
 } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
 
-import {
-    getStorage
-} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js";
-
-
-// ==========================================
-// FIREBASE CONFIG
-// ==========================================
-
 const firebaseConfig = {
 
     apiKey: "AIzaSyCSxn1dAA708KyKOfrLP_nTrBnazrFjPT8",
 
-    authDomain: "kanakpur-tarun-sangha-42e7b.firebaseapp.com",
+    authDomain:
+        "kanakpur-tarun-sangha-42e7b.firebaseapp.com",
 
-    projectId: "kanakpur-tarun-sangha-42e7b",
+    projectId:
+        "kanakpur-tarun-sangha-42e7b",
 
-    storageBucket: "kanakpur-tarun-sangha-42e7b.firebasestorage.app",
+    storageBucket:
+        "kanakpur-tarun-sangha-42e7b.firebasestorage.app",
 
-    messagingSenderId: "1062615905207",
+    messagingSenderId:
+        "1062615905207",
 
-    appId: "1:1062615905207:web:53e520f0696aeb0daa6e32"
-
+    appId:
+        "1:1062615905207:web:53e520f0696aeb0daa6e32"
 };
 
-
-// ==========================================
-// INITIALIZE FIREBASE
-// ==========================================
 
 const app = initializeApp(firebaseConfig);
 
-
-// ==========================================
-// INITIALIZE FIRESTORE
-// ==========================================
-
 const db = getFirestore(app);
 
-
-// ==========================================
-// INITIALIZE STORAGE
-// ==========================================
-
-const storage = getStorage(app);
-
-
-// ==========================================
-// EXPORT
-// ==========================================
-
-export {
-    db,
-    storage
-};
+export { db };
