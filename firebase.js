98e882a826c57d03df46917040027c184c09cb6c
kanakpurts1965 <kanakpurts1965@gmail.com>
2026-07-03 17:33:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,33 +3,19 @@
 // FIRESTORE + STORAGE
 // ==========================================
 
-
-// Firebase App
-
 import {
-
     initializeApp
-
 } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
 
 
-// Firestore
-
 import {
-
     getFirestore
-
 } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
 
-// Firebase Storage
-
 import {
-
     getStorage
-
 } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js";
-
 
 
 // ==========================================
@@ -38,53 +24,44 @@ import {
 
 const firebaseConfig = {
 
-    apiKey:
-        "AIzaSyCSxn1dAA708KyKOfrLP_nTrBnazrFjPT8",
+    apiKey: "AIzaSyCSxn1dAA708KyKOfrLP_nTrBnazrFjPT8",
 
-    authDomain:
-        "kanakpur-tarun-sangha-42e7b.firebaseapp.com",
+    authDomain: "kanakpur-tarun-sangha-42e7b.firebaseapp.com",
 
-    projectId:
-        "kanakpur-tarun-sangha-42e7b",
+    projectId: "kanakpur-tarun-sangha-42e7b",
 
-    storageBucket:
-        "kanakpur-tarun-sangha-42e7b.firebasestorage.app",
+    storageBucket: "kanakpur-tarun-sangha-42e7b.firebasestorage.app",
 
-    messagingSenderId:
-        "1062615905207",
+    messagingSenderId: "1062615905207",
 
-    appId:
-        "1:1062615905207:web:53e520f0696aeb0daa6e32"
+    appId: "1:1062615905207:web:53e520f0696aeb0daa6e32"
 
 };
-
 
 
 // ==========================================
 // INITIALIZE FIREBASE
 // ==========================================
 
-const app =
-    initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig);
 
 
 // ==========================================
 // INITIALIZE FIRESTORE
 // ==========================================
 
-const db =
-    getFirestore(app);
-
+const db = getFirestore(app);
 
 
 // ==========================================
 // INITIALIZE STORAGE
 // ==========================================
 
-const storage =
-   // ==========================================
-// EXPORT FIREBASE SERVICES
+const storage = getStorage(app);
+
+
+// ==========================================
+// EXPORT
 // ==========================================
 
 export {
