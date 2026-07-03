8ac4ba643ee66ac0724f26b42f681487b56628e4
kanakpurts1965 <kanakpurts1965@gmail.com>
2026-07-03 17:21:22 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,86 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+// ==========================================
+// FIREBASE CONFIGURATION
+// FIRESTORE + STORAGE
+// ==========================================
+
+
+// Firebase App
+
+import {
+
+    initializeApp
+
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+
+
+// Firestore
+
+import {
+
+    getFirestore
+
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+
+
+// Firebase Storage
+
+import {
+
+    getStorage
+
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js";
+
+
+
+// ==========================================
+// FIREBASE CONFIG
+// ==========================================
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSxn1dAA708KyKOfrLP_nTrBnazrFjPT8",
-  authDomain: "kanakpur-tarun-sangha-42e7b.firebaseapp.com",
-  projectId: "kanakpur-tarun-sangha-42e7b",
-  storageBucket: "kanakpur-tarun-sangha-42e7b.firebasestorage.app",
-  messagingSenderId: "1062615905207",
-  appId: "1:1062615905207:web:53e520f0696aeb0daa6e32"
+
+    apiKey:
+        "AIzaSyCSxn1dAA708KyKOfrLP_nTrBnazrFjPT8",
+
+    authDomain:
+        "kanakpur-tarun-sangha-42e7b.firebaseapp.com",
+
+    projectId:
+        "kanakpur-tarun-sangha-42e7b",
+
+    storageBucket:
+        "kanakpur-tarun-sangha-42e7b.firebasestorage.app",
+
+    messagingSenderId:
+        "1062615905207",
+
+    appId:
+        "1:1062615905207:web:53e520f0696aeb0daa6e32"
+
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
-const db = getFirestore(app);
 
-// Export Firestore
-export { db };
+// ==========================================
+// INITIALIZE FIREBASE
+// ==========================================
+
+const app =
+    initializeApp(firebaseConfig);
+
+
+
+// ==========================================
+// INITIALIZE FIRESTORE
+// ==========================================
+
+const db =
+    getFirestore(app);
+
+
+
+// ==========================================
+// INITIALIZE STORAGE
+// ==========================================
+
+const storage =
+   
