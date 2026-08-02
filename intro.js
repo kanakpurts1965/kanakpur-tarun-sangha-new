ef43f1fa2a33546b102b6bddb3eb45aba9815d01
kanakpurts1965 <kanakpurts1965@gmail.com>
2026-08-02 14:54:08 +0530

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,3 +1,13 @@
-setTimeout(() => {
-    window.location.href = "index.html";
-},3000);
+window.addEventListener("load", () => {
+
+    document.body.classList.add("show");
+
+    setTimeout(() => {
+        document.body.classList.add("hide");
+    }, 2500);
+
+    setTimeout(() => {
+        window.location.replace("index.html");
+    }, 3200);
+
+});
