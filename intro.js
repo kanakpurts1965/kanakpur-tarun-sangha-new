020f3583fd3b2be9ccf16e0224e29155706a8add
kanakpurts1965 <kanakpurts1965@gmail.com>
2026-08-02 15:07:40 +0530

    Update intro.js

--- intro.js
+++ intro.js
@@ -28,3 +28,15 @@ clearInterval(timer);
 }
 
 },30);
+setTimeout(()=>{
+
+document.querySelector(".intro").style.transform="scale(.95)";
+document.querySelector(".intro").style.opacity="0";
+
+},2600);
+
+setTimeout(()=>{
+
+window.location.replace("index.html");
+
+},3200);
