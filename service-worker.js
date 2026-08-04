28646006600f2a7014915ed7f87e896d027ca58f
kanakpurts1965 <kanakpurts1965@gmail.com>
2026-08-04 11:47:43 +0530

    Update cache version and cached URLs in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,19 @@
-const CACHE_NAME = "kts-v3";
+const CACHE_NAME = "kts-v4";
 
 const urlsToCache = [
- "/",
-"/index.html",
-"/style.css",
-"/dashboard.html"
+  "/",
+  "/index.html",
+  "/about.html",
+  "/members.html",
+  "/events.html",
+  "/gallery.html",
+  "/contact.html",
+  "/notices.html",
+
+  "/style.css",
+  "/script.js",
+  "/gallery.js",
+  "/firebase.js",
+
+  "/123.png.png"
 ];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
